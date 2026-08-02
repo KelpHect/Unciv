67b725f4bbbe0a769fda88e68d17912dfb5c69b7
yairm210 <yairm210@hotmail.com>
2026-08-02 16:58:59 +0300

    View implementation - Separate CityView.viewingCiv() from CityView.owningCiv() - see #15280

--- core/src/com/unciv/view/TileView.kt
+++ core/src/com/unciv/view/TileView.kt
@@ -20,6 +20,9 @@ class TileView(private val tile: Tile, private val viewer: Civilization) {
         tile.tilesInDistanceSequence(distance).map { TileView(it, viewer) }
 
     @Readonly fun isCityCenter(): Boolean = tile.isCityCenter()
+    @Readonly fun isWorked(): Boolean = tile.isWorked()
+    @Readonly fun isBlockaded(): Boolean = tile.isBlockaded()
+    @Readonly fun providesYield(): Boolean = tile.providesYield()
     @Readonly fun isLocked(): Boolean = tile.isLocked()
     @Readonly fun isImpassible(): Boolean = tile.isImpassible()
     @Readonly fun isAdjacentTo(terrainFilter: String): Boolean = tile.isAdjacentTo(terrainFilter)
